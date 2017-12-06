c939cf29d22b910d4eaddb6295b16fa851aff366
jurij.mackert <mackert.jurij@gmail.com>
2017-12-06 17:00:48 +0100

    Right Side scrolling on Left Side.scroll

--- js/main.js
+++ js/main.js
@@ -4,12 +4,9 @@ $(document).ready(function(){
     var $container_left_Height = $container_left.outerHeight();
     var $container_right_Height = $container_right.outerHeight();
     var top_1 = 0;
-
-    console.log("Jquery imported");
-    console.log("Container Height at start:" + $container_left_Height);
+    var top_2 = 0;
 
     var scrollerLeft = function(event) {
-        console.log("scroller Left called");
         top_1 += $container_left_Height * (($(this).scrollTop() > top_1) ? 1 : -1);
         $container_left.animate({
             scrollTop: top_1
@@ -20,29 +17,17 @@ $(document).ready(function(){
             });
         });
 
-        return;
-    }
-
-    var scrollerRight = function(event) {
-        console.log("scroller Right called");
-        top_1 += $container_right_Height * (($(this).scrollTop() > top_1) ? 1 : -1);
+        top_2 += $container_right_Height * (($(this).scrollTop() > top_2) ? 1 : -1);
         $container_right.animate({
-            scrollTop: top_1
-        }, 500, 'linear', function() {
-            $(this).clearQueue().one("scroll", function() {
-                // nesting eats the last event so it doesn't self-trigger
-                $(this).one("scroll", scrollerRight);
-            });
-        });
+            scrollTop: top_2
+        }, 500, 'linear');
+
         return;
     }
-
 
     $container_left.one("scroll", scrollerLeft);
-    $container_right.one("scroll", scrollerRight);
 
     $(window).on('resize', function(){
-        console.log("Screen was resized");
         $container_left_Height = $container_left.outerHeight();
         $container_right_Height = $container_right.outerHeight();
         console.log("Container Height after resize:" + $container_left_Height);
