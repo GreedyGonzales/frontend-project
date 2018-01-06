71db2222fa81b7e991b24e0a3404487be74d6c94
jurij.mackert <mackert.jurij@gmail.com>
2018-01-06 14:55:36 +0100

    Alle Bereiche der rechten Seite übereinandergelegt.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
 $(document).ready(function(){
     var $container_left = $('#container-left');
-    //      var $container_right = $('#container-right');
+    var $container_right = $('#container-right');
     var $container_left_Height = $container_left.outerHeight();
-    //      var $container_right_Height = $container_right.outerHeight();
     var top_1 = 0;
-    //      var top_2 = 0;
-
-    //      $container_right.scrollTop(2904);
-
-    //      console.log("Left Container - Default Scrollbar Position:" + $container_left.scrollTop());
-    //      console.log("Right Container - Default Scrollbar Position:" + $container_right.scrollTop());
-
 
     var scrollerLeft = function() {
         top_1 += $container_left_Height * (($(this).scrollTop() > top_1) ? 1 : -1);
@@ -22,33 +14,16 @@ $(document).ready(function(){
                 $(this).one("scroll", scrollerLeft);
             });
         });
-
     };
 
-
-    //      console.log("this.scrollTop: " + $container_right.scrollTop());
-        //      console.log("top_2 vor der Berechnung: " + top_2);
-        /*
-        top_2 -= $container_right_Height * (($container_right.scrollTop() > top_2) ? 1 : -1);
-        $container_right.animate({
-            scrollTop: top_2
-        }, 800, 'linear');
-
-        console.log("top_2 nach der Berechnung: " + top_2);
-        console.log("Left Container - End Scrollbar Position:" + $container_left.scrollTop());
-        console.log("Right Container - End Scrollbar Position:" + $container_right.scrollTop());
-
-
-         */
-
-
-        //return;
+    var displayImages = function() {
+        console.log("Funktionstest");
+    };
 
     $container_left.one("scroll", scrollerLeft);
+    $container_left.one("scroll", displayImages);
 
     $(window).on('resize', function(){
         $container_left_Height = $container_left.outerHeight();
-        //      $container_right_Height = $container_right.outerHeight();
-        //      console.log("Container Height after resize:" + $container_left_Height);
     });
 });
